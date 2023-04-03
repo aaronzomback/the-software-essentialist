3f5dc6140380e81a1fa0442a3d8bb4e5d3a9e079
Aaron Zomback <aaronzomback@gmail.com>
2023-04-04 01:16:55 +0200

    [refactor]: using nested describe block

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,14 +1,22 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("takes numbers from 1 to 100", () => {
-    expect(() => fizzbuzz(1)).not.toThrow();
-    expect(() => fizzbuzz(100)).not.toThrowError();
-    expect(() => fizzbuzz(0)).toThrowError(
-      "Number must be between 1 to 100 inclusive."
-    );
-    expect(() => fizzbuzz(101)).toThrowError(
-      "Number must be between 1 to 100 inclusive."
-    );
+  describe("takes numbers from 1 to 100", () => {
+    it("knows that 1 is a valid input because it's between 1 & 100", () => {
+      expect(() => fizzbuzz(1)).not.toThrow();
+    });
+    it("knows that 100 is a valid input because it's between 1 & 100", () => {
+      expect(() => fizzbuzz(100)).not.toThrowError();
+    });
+    it("knows that 0 is an invalid input because it's NOT between 1 & 100", () => {
+      expect(() => fizzbuzz(0)).toThrowError(
+        "Number must be between 1 to 100 inclusive."
+      );
+    });
+    it("knows that 101 is an invalid input because it's NOT between 1 & 100", () => {
+      expect(() => fizzbuzz(101)).toThrowError(
+        "Number must be between 1 to 100 inclusive."
+      );
+    });
   });
 });
