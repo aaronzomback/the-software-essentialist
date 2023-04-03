59c0693b533d31c3953425e60ee5145e5a564a23
Aaron Zomback <aaronzomback@gmail.com>
2023-04-04 01:06:39 +0200

    [feat] - test constraint on number input

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,5 @@
+export const fizzbuzz = (number: number) => {
+  if (number < 1 || number > 100)
+    throw new Error("Number must be between 1 to 100 inclusive.");
+  return;
+};
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,14 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("takes numbers from 1 to 100", () => {
+    expect(() => fizzbuzz(1)).not.toThrow();
+    expect(() => fizzbuzz(100)).not.toThrowError();
+    expect(() => fizzbuzz(0)).toThrowError(
+      "Number must be between 1 to 100 inclusive."
+    );
+    expect(() => fizzbuzz(101)).toThrowError(
+      "Number must be between 1 to 100 inclusive."
+    );
+  });
 });
