b83dce614f61ca89ce47154a46beee964345190d
Aaron Zomback <aaronzomback@gmail.com>
2023-04-26 23:05:06 +0200

    [refactor] - extract methods for conditionals

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,8 +1,28 @@
 export const fizzbuzz = (number: number) => {
+  validateNumberInput(number);
+  if (!isMultitpleOf3or5(number)) return number.toString();
+  if (isOnlyMultipleOf3(number)) return "Fizz";
+  if (isOnlyMultipleOf5(number)) return "Buzz";
+  if (isMultipleOf3and5(number)) return "FizzBuzz";
+};
+
+const validateNumberInput = (number: number) => {
   if (number < 1 || number > 100)
     throw new Error("Number must be between 1 to 100 inclusive.");
-  if (!(number % 3 === 0 || number % 5 === 0)) return number.toString();
-  if (number % 3 === 0 && number % 5 !== 0) return "Fizz";
-  if (number % 5 === 0 && number % 3 !== 0) return "Buzz";
-  if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
+};
+
+const isMultitpleOf3or5 = (number: number) => {
+  return number % 3 === 0 || number % 5 === 0;
+};
+
+const isOnlyMultipleOf3 = (number: number) => {
+  return number % 3 === 0 && number % 5 !== 0;
+};
+
+const isOnlyMultipleOf5 = (number: number) => {
+  return number % 5 === 0 && number % 3 !== 0;
+};
+
+const isMultipleOf3and5 = (number: number) => {
+  return number % 3 === 0 && number % 5 === 0;
 };
