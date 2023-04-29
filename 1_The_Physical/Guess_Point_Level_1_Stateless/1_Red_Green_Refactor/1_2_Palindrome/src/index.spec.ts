87a571d9573a893b4f680528e177e9ed8ef933ee
Aaron Zomback <aaronzomback@gmail.com>
2023-04-29 23:30:02 +0200

    [feat]: returns hardcoded true for all single phrase palindromes

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,21 @@ describe("palindrome checker", () => {
   it("should be defined", () => {
     expect(isPalindrome).toBeDefined();
   });
+
+  it.each([
+    "racecar",
+    "mom",
+    "dad",
+    "noon",
+    "madam",
+    "civic",
+    "kayak",
+    "level",
+    "rotor",
+    "stats",
+    "tenet",
+    "wow",
+  ])("should return true for %s", (subject) => {
+    expect(isPalindrome(subject)).toBe(true);
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,3 @@
 export function isPalindrome(subject: string) {
-    
-
+  return true;
 }
