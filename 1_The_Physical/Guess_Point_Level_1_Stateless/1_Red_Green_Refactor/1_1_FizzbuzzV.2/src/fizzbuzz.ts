d8105095ab61e743535804c81d90fad004a951d7
Aaron Zomback <aaronzomback@gmail.com>
2023-05-20 19:19:23 +0200

    [red to green] - throws correct NonNumber type error when input is not a number

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export const fizzbuzz = (n: number): string => {
+    if (typeof n !== "number") throw NonNumberTypeError
     if (n < 1 || n > 100) throw NumberOutOfBoundsError
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz" // this must be first because it is more specific than the other two (i.e. 15 is a multiple of 3 and 5, but it is also a multiple of 3 and 5
     if (n % 3 === 0) return "Fizz"
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/index.spec.ts
@@ -2,7 +2,7 @@ import { NonNumberTypeError, NumberOutOfBoundsError, fizzbuzz } from "./fizzbuzz
 
 describe('fizzbuzz', () => {
     describe("takes numbers from 1 to 100 and outputs them as a string", () => {
-        it.each([[1, "1"], [2, "2"], [4, "4"], [7, "7"], [11, "11"], [13, "13"], [17, "17"], [98, "98"]])("when given %s it returns '%s'", (input, expected) => {
+        it.each([[1, "1"], [2, "2"], [2.6, "2.6"], [4, "4"], [7, "7"], [11, "11"], [13, "13"], [17, "17"], [98, "98"]])("when given %s it returns '%s'", (input, expected) => {
             expect(fizzbuzz(input)).toBe(expected)
         })
     });
@@ -18,7 +18,7 @@ describe('fizzbuzz', () => {
     })
     describe('returns "Buzz" when given a multiple of 5', () => {
         it.each([[5, "Buzz"], [10, "Buzz"], [20, "Buzz"], [25, "Buzz"], [35, "Buzz"], [40, "Buzz"], [50, "Buzz"], [55, "Buzz"], [65, "Buzz"], [70, "Buzz"], [80, "Buzz"], [85, "Buzz"], [95, "Buzz"], [100, "Buzz"]])("when given %s it returns '%s'", (input, expected) => {
-            expect(fizzbuzz(input)).toBe(expected)
+            expect(fizzbuzz(input)).toBe("Buzz")
         })
     })
     describe('returns "FizzBuzz" when given a multiple of 3 and 5', () => {
@@ -28,7 +28,13 @@ describe('fizzbuzz', () => {
     })
     describe('gracefully handles non-numbers', () => {
         it("when given a string it throws an error", () => {
-            expect(() => fizzbuzz("string")).toThrowError(NonNumberTypeError.message)
+            expect(() => fizzbuzz("string" as any)).toThrowError(NonNumberTypeError.message)
         })
+        it("when given an array it throws an error", () => {
+           expect(() => fizzbuzz([] as any)).toThrowError(NonNumberTypeError.message);
+         });
+        it("when given a boolean it throws an error", () => {
+          expect(() => fizzbuzz(true as any)).toThrowError(NonNumberTypeError.message);
+        });                   
     })
 })
