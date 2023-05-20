3c12004ba59dda2a3f4afdc0b211285843935d8e
Aaron Zomback <aaronzomback@gmail.com>
2023-05-20 18:10:51 +0200

    [green to red] - fails to return '2' when given number 2

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/index.spec.ts
@@ -5,5 +5,8 @@ describe('fizzbuzz', () => {
         it("returns '1' when given 1", () => {
         expect(fizzbuzz(1)).toBe("1")
         })
+        it("returns '2' when given 2", () => {
+        expect(fizzbuzz(2)).toBe("2")
+        })
     });
 })
