574a4f74f65392aed569ef0016cb3ada711826ea
Aaron Zomback <aaronzomback@gmail.com>
2023-05-20 19:27:06 +0200

    [green to refactor] - extract helper functions with more expressive names

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_FizzbuzzV.2/src/fizzbuzz.ts
@@ -1,12 +1,16 @@
 export const fizzbuzz = (n: number): string => {
     if (typeof n !== "number") throw NonNumberTypeError
     if (n < 1 || n > 100) throw NumberOutOfBoundsError
-    if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz" // this must be first because it is more specific than the other two (i.e. 15 is a multiple of 3 and 5, but it is also a multiple of 3 and 5
-    if (n % 3 === 0) return "Fizz"
-    if (n % 5 === 0) return "Buzz"
+    if (isMultipleOfThreeAndFive(n)) return "FizzBuzz" // this must be first because it is more specific than the other two (i.e. 15 is a multiple of 3 and 5, but it is also a multiple of 3 and 5
+    if (isMultipleOfThree(n)) return "Fizz"
+    if (isMultipleOfFive(n)) return "Buzz"
     return n.toString()
 }
 
 export const NumberOutOfBoundsError = new Error("Input must be a number between 1 and 100 inclusive");
 
 export const NonNumberTypeError = new Error("Input must be of type number");
+
+const isMultipleOfThree = (n: number): boolean => n % 3 === 0
+const isMultipleOfFive = (n: number): boolean => n % 5 === 0
+const isMultipleOfThreeAndFive = (n: number): boolean => isMultipleOfThree(n) && isMultipleOfFive(n)
