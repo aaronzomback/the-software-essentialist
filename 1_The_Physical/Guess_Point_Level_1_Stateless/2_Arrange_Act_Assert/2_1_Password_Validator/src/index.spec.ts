e80fcac2bf96f3fa7c3ce96b1e450309f2b0c8b7
Aaron Zomback <aaronzomback@gmail.com>
2023-05-22 01:14:59 +0200

    [refactor] assert specific properties of responseObj

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -12,45 +12,41 @@ describe("password validator", () => {
     it.each(["thePhysical1234567", "Ho2", "Mo1", "Mo1".repeat(6)])(
       "should should return an error if the password length is not between 5 or 15 characters, i.e. %s",
       (input) => {
-        responseObj = {
-          valid: false,
-          errors: [InvalidLengthError],
-        };
-        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+        responseObj = PasswordValidator.validate(input);
+        expect(responseObj.valid).toEqual(false);
+        expect(responseObj.errors.length).toBe(1);
+        expect(responseObj.errors).toEqual([InvalidLengthError]);
       }
     );
     it.each(["maxwell1_c", "password1", "yoyo1", "supersecret2"])(
       "should return an error if the password does not contain an uppercase letter, i.e. %s",
       (input) => {
-        responseObj = {
-          valid: false,
-          errors: [InvalidUpperCaseError],
-        };
-        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+        responseObj = PasswordValidator.validate(input);
+        expect(responseObj.valid).toEqual(false);
+        expect(responseObj.errors.length).toBe(1);
+        expect(responseObj.errors).toEqual([InvalidUpperCaseError]);
       }
     );
     it.each(["maxwellTheBe", "Mo".repeat(6), "Password", "SuperSecret"])(
       "should return an error if the password does not contain a digit, i.e. %s",
       (input) => {
-        responseObj = {
-          valid: false,
-          errors: [InvalidDigitError],
-        };
-        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+        responseObj = PasswordValidator.validate(input);
+        expect(responseObj.valid).toEqual(false);
+        expect(responseObj.errors.length).toBe(1);
+        expect(responseObj.errors).toEqual([InvalidDigitError]);
       }
     );
     it.each(["low", "yo", "kno"])(
       "should return multiple errors if the password is not valid for multiple criterias",
       (input) => {
-        responseObj = {
-          valid: false,
-          errors: [
-            InvalidLengthError,
-            InvalidUpperCaseError,
-            InvalidDigitError,
-          ],
-        };
-        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+        responseObj = PasswordValidator.validate(input);
+        expect(responseObj.valid).toBe(false);
+        expect(responseObj.errors.length).toBe(3);
+        expect(responseObj.errors).toEqual([
+          InvalidLengthError,
+          InvalidUpperCaseError,
+          InvalidDigitError,
+        ]);
       }
     );
   });
