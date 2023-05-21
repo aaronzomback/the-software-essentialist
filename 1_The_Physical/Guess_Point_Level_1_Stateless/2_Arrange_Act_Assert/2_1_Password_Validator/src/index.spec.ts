810d7ea493971bff832ff0507c19f9f0e84b486a
Aaron Zomback <aaronzomback@gmail.com>
2023-05-22 00:47:04 +0200

    [refactor] rule of 3 it.each for each test and further define responsibilities

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -9,34 +9,35 @@ import {
 describe("password validator", () => {
   let responseObj: ResponseObject;
   describe("when the password is not valid", () => {
-    it("should return an error if the password does not contain an uppercase letter", () => {
-      responseObj = {
-        valid: false,
-        errors: [InvalidUpperCaseError],
-      };
-      expect(PasswordValidator.validate("password1")).toEqual(responseObj);
-    });
-    it("should should return an error if the password length is not between 5 or 15 characters", () => {});
-    it("should return an error if the password is too short", () => {
-      responseObj = {
-        valid: false,
-        errors: [InvalidLengthError],
-      };
-      expect(PasswordValidator.validate("Mo1")).toEqual(responseObj);
-    });
-    it("should return an error if the password is too long", () => {
-      responseObj = {
-        valid: false,
-        errors: [InvalidLengthError],
-      };
-      expect(PasswordValidator.validate("Mo1".repeat(6))).toEqual(responseObj);
-    });
-    it("should return an error if the password does not contain a digit", () => {
-      responseObj = {
-        valid: false,
-        errors: [InvalidDigitError],
-      };
-      expect(PasswordValidator.validate("Mo".repeat(6))).toEqual(responseObj);
-    });
+    it.each(["Ho2", "Mo1", "Mo1".repeat(6)])(
+      "should should return an error if the password length is not between 5 or 15 characters, i.e. %s",
+      (input) => {
+        responseObj = {
+          valid: false,
+          errors: [InvalidLengthError],
+        };
+        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+      }
+    );
+    it.each(["password1", "yoyo1", "supersecret2"])(
+      "should return an error if the password does not contain an uppercase letter, i.e. %s",
+      (input) => {
+        responseObj = {
+          valid: false,
+          errors: [InvalidUpperCaseError],
+        };
+        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+      }
+    );
+    it.each(["Mo".repeat(6), "Password", "SuperSecret"])(
+      "should return an error if the password does not contain a digit, i.e. %s",
+      (input) => {
+        responseObj = {
+          valid: false,
+          errors: [InvalidDigitError],
+        };
+        expect(PasswordValidator.validate(input)).toEqual(responseObj);
+      }
+    );
   });
 });
