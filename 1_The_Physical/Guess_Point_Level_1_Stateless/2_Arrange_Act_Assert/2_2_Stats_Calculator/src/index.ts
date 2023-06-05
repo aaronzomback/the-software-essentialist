39155e12f5e804c1fcf0c904663720248fb50007
Aaron Zomback <aaronzomback@gmail.com>
2023-06-06 00:11:18 +0200

    refactor: extract private helper method getMax - real implementation

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -11,10 +11,15 @@ export class JukeboxMath {
       throw new Error("Empty array");
     }
     const min = this.getMin(numbers);
-    return { min, max: 2 } as JukeboxResponse;
+    const max = this.getMax(numbers);
+    return { min, max } as JukeboxResponse;
   }
 
   private static getMin(numbers: number[]): number {
     return numbers.reduce((acc, curr) => (acc < curr ? acc : curr), numbers[0]);
+  }
+
+  private static getMax(numbers: number[]): number {
+    return numbers.reduce((acc, curr) => (acc > curr ? acc : curr), numbers[0]);
   }
 }
